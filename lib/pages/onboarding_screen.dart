311b65592402daa31992e27226fdc38de17c542a
AetherZelta <isaacbravom10@gmail.com>
2024-03-01 17:50:27 -0600

    Cambios en onboarding

--- lib/pages/onboarding_screen.dart
+++ lib/pages/onboarding_screen.dart
@@ -1,5 +1,7 @@
+import 'package:blurrycontainer/blurrycontainer.dart';
 import 'package:flutter/material.dart';
 import 'package:genshin_app/pages/home_page.dart';
+import 'package:lottie/lottie.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class OnBoardingScreen extends StatefulWidget {
@@ -32,13 +34,108 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
                     fit: BoxFit.fitWidth,
                   ),
                 ),
-                //color: Colors.blue,
+                child: Center(
+                  child: BlurryContainer(
+                    color: Colors.white.withOpacity(0.15),
+                    blur: 6,
+                    elevation: 6,
+                    borderRadius: BorderRadius.circular(20),
+                    child: SizedBox(
+                      height: 580,
+                      width: 280,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20),
+                            child: SizedBox(
+                              width: 200,
+                              height: 200,
+                              child:
+                                  Lottie.asset('assets/animations/game.json'),
+                            ),
+                          ),
+                          const Text(
+                            'Preparate para tu aventura',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                              fontSize: 18,
+                            ),
+                          ),
+                          const Padding(
+                            padding:
+                                EdgeInsets.only(top: 40, left: 10, right: 10),
+                            child: Text(
+                              'Si haz comenzado tu viaje por Teyvat hace poco ' +
+                                  'la siguiente aplicacion te dara los detalles necesarios para que conozcas mejor ' +
+                                  'los detalles del mundo que exploraras.',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                color: Colors.white,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
               ),
               Container(
                 decoration: const BoxDecoration(
                   image: DecorationImage(
                     image: AssetImage('assets/images/aetherwalk.png'),
                     fit: BoxFit.fitWidth,
+                  ),
+                ),
+                child: Center(
+                  child: BlurryContainer(
+                    color: Colors.white.withOpacity(0.15),
+                    blur: 6,
+                    elevation: 6,
+                    borderRadius: BorderRadius.circular(20),
+                    child: SizedBox(
+                      height: 580,
+                      width: 280,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20),
+                            child: SizedBox(
+                              width: 200,
+                              height: 200,
+                              child:
+                                  Lottie.asset('assets/animations/fight.json'),
+                            ),
+                          ),
+                          const Text(
+                            'Mejora tus equipos',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                              fontSize: 18,
+                            ),
+                          ),
+                          const Padding(
+                            padding:
+                                EdgeInsets.only(top: 40, left: 10, right: 10),
+                            child: Text(
+                              'Si haz comenzado tu viaje por Teyvat hace poco ' +
+                                  'la siguiente aplicacion te dara los detalles necesarios para que conozcas mejor ' +
+                                  'los detalles del mundo que exploraras.',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                color: Colors.white,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 ),
                 //color: Colors.yellow,
@@ -50,12 +147,57 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
                     fit: BoxFit.fitWidth,
                   ),
                 ),
+                child: Center(
+                  child: BlurryContainer(
+                    color: Colors.white.withOpacity(0.15),
+                    blur: 6,
+                    elevation: 6,
+                    borderRadius: BorderRadius.circular(20),
+                    child: SizedBox(
+                      height: 580,
+                      width: 280,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20),
+                            child: SizedBox(
+                              width: 200,
+                              height: 200,
+                              child: Lottie.asset(
+                                  'assets/animations/success.json'),
+                            ),
+                          ),
+                          const Text(
+                            'Teyvat',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                              fontSize: 18,
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(top: 40),
+                            child: Text(
+                              'Preparate para tu aventura',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                color: Colors.white,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
                 //color: Colors.green,
               ),
             ],
           ),
           Container(
-            alignment: Alignment(0, 0.75),
+            alignment: const Alignment(0, 0.90),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
@@ -63,10 +205,16 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
                   onTap: () {
                     _controller.jumpToPage(2);
                   },
-                  child: const Text(
-                    'saltar',
-                    style: TextStyle(
-                      color: Colors.black,
+                  child: BlurryContainer(
+                    color: Colors.white.withOpacity(0.20),
+                    blur: 8,
+                    elevation: 6,
+                    borderRadius: BorderRadius.circular(10),
+                    child: const Text(
+                      'saltar',
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
                     ),
                   ),
                 ),
@@ -83,10 +231,16 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
                             ),
                           );
                         },
-                        child: const Text(
-                          'hecho',
-                          style: TextStyle(
-                            color: Colors.black,
+                        child: BlurryContainer(
+                          color: Colors.white.withOpacity(0.20),
+                          blur: 8,
+                          elevation: 6,
+                          borderRadius: BorderRadius.circular(10),
+                          child: const Text(
+                            'hecho',
+                            style: TextStyle(
+                              color: Colors.black,
+                            ),
                           ),
                         ),
                       )
@@ -97,10 +251,16 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
                             curve: Curves.easeIn,
                           );
                         },
-                        child: const Text(
-                          'siguiente',
-                          style: TextStyle(
-                            color: Colors.black,
+                        child: BlurryContainer(
+                          color: Colors.white.withOpacity(0.20),
+                          blur: 8,
+                          elevation: 6,
+                          borderRadius: BorderRadius.circular(10),
+                          child: const Text(
+                            'siguiente',
+                            style: TextStyle(
+                              color: Colors.black,
+                            ),
                           ),
                         ),
                       ),
